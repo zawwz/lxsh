39c8f7fa589061a53be41308bca4f4988d9de955
zawz <zawz@hotmail.fr>
2021-02-28 15:11:19 +0100

    fix debashified [*] on maps having extra space

--- shellcode/map_get.sh
+++ shellcode/map_get.sh
@@ -1,6 +1,6 @@
 __lxsh_map_get() {
   if [ "$2" = \* ] || [ "$2" = @ ] ; then
-    printf "%s\n" "$1" | sort | cut -d ']' -f2-
+    printf "%s" "$(printf "%s" "$1" | sort | cut -d ']' -f2-)" | tr '\n' ' '
   else
     printf "%s\n" "$1" | grep "^$2\]" | cut -d ']' -f2-
   fi
