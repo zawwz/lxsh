2d0041e1ff4d62fdf657ce8ba5c2af6454be6f15
zawz <zawz@hotmail.fr>
2021-10-20 17:30:11 +0200

    fix broken heredocument

--- src/generate.cpp
+++ src/generate.cpp
@@ -90,6 +90,7 @@ std::string condlist::generate(int ind)
       ret += '&';
     ret += '\n';
     ret += ctx.here_document->generate(0);
+    ret += '\n';
     prev_is_heredoc=true;
   }
   else if(parallel)
